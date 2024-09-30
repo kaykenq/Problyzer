6d452f91645cb004062bb453c9e4607ce01b2da0
kaykenq <kaykee.pn@gmail.com>
2024-09-30 11:17:01 -0300

    ...

--- src/managers/Datas.ts
+++ src/managers/Datas.ts
@@ -1,11 +1,13 @@
 import Data from "../structures/bases/Data"
-
+import { DataOptions } from "../structures/interfaces/data"
+// isso aqui é para salvar arquivos, por exemplo resultados, além disso, isso aplicará alguns dos algoritimos
 export default class DataManager extends Data {
+    public options?: DataOptions;
     constructor(data: Record<(string | number), (string | number)>) {
         super(data)
     }
 
-    public () {
-        
+    public applyAlgorithm(options: DataOptions) {
+        this.options = options
     }
 }
